36f8b35ccb307881244fba671f0e986bb4881df1
LuoZhize <1518879327@qq.com>
2018-12-12 17:01:49 +0800

    list & axios & loading

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -3,15 +3,24 @@
       <div class="slider-group" ref="sliderGroup">
         <slot></slot>
       </div>
-      <div class="dots"></div>
+      <div class="dots">
+        <span class="dot" v-for="(item, index) in dots" :key="index" :class="{active: currentPageIndex === index}"></span>
+      </div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
 import BScroll from 'better-scroll'
-import {addClass} from '../../common/js/dom'
+import {addClass} from 'common/js/dom'
 
 export default {
+  name: 'slider',
+  data() {
+    return {
+      dots: [],
+      currentPageIndex: 0
+    }
+  },
   props: {
     loop: {
       type: Boolean,
@@ -29,11 +38,25 @@ export default {
   mounted() {
     setTimeout(() => {
       this._setSLiderWidth()
+      this._initDots()
       this._initSlider()
+      if (this.autoPlay) {
+        this._play()
+      }
     }, 20)
+    window.addEventListener('resize', () => {
+      if (!this.slider) {
+        return
+      }
+      this._setSLiderWidth(true)
+      this.slider.refresh()
+    })
+  },
+  destroyed() {
+    clearTimeout(this.timer)
   },
   methods: {
-    _setSLiderWidth() {
+    _setSLiderWidth(isResize) {
       this.children = this.$refs.sliderGroup.children
       let width = 0
       let sliderWidth = this.$refs.slider.clientWidth
@@ -43,22 +66,42 @@ export default {
         child.style.width = sliderWidth + 'px'
         width += sliderWidth
       }
-      if (this.loop) {
+      if (this.loop && !isResize) {
         width += 2 * sliderWidth
       }
       this.$refs.sliderGroup.style.width = width + 'px'
     },
+    _initDots() {
+      this.dots = new Array(this.children.length)
+    },
     _initSlider() {
+      const option = {
+        click: true
+      }
       this.slider = new BScroll(this.$refs.slider, {
         scrollX: true,
         scrollY: false,
         momentum: false,
-        snap: true,
-        snapLoop: this.loop,
-        snapThreshold: 0.3,
-        snapSpeed: 400,
-        click: true
+        click: true,
+        snap: {
+          loop: this.loop,
+          threshold: 0.3,
+          speed: 400
+        }
+      }, option)
+      this.slider.on('scrollEnd', () => {
+        let pageIndex = this.slider.getCurrentPage().pageX
+        this.currentPageIndex = pageIndex
+        if (this.autoPlay) {
+          clearTimeout(this.timer)
+          this._play()
+        }
       })
+    },
+    _play() {
+      this.timer = setTimeout(() => {
+        this.slider.next()
+      }, this.interval)
     }
   }
 }
@@ -82,9 +125,9 @@ export default {
           width 100%
           overflow hidden
           text-decoration none
-          img
-            display block
-            width 100%
+        img
+          display block
+          width 100%
     .dots
       position absolute
       right 0
